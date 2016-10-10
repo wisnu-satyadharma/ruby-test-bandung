594db8cedd1f52f8f78c3a8c7725518cd18c3d5c
wisnu <wisnu_dagonza8@yahoo.co.id>
2016-10-10 09:23:29 +0700

    remove debugging codes

--- backupapp/app/controllers/attachments_controller.rb
+++ backupapp/app/controllers/attachments_controller.rb
@@ -1,6 +1,5 @@
 class AttachmentsController < ApplicationController
   def show_contain
   	@attachment = Attachment.find_by(id: params[:id])  	
-  	puts Paperclip.io_adapters.for(@attachment.item).read
   end
 end
